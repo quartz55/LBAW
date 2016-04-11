a8578d4a8a5654180562220e6b9f8806b2c3f4a2
joao <joao88vsc@gmail.com>
2016-04-11 17:00:57 +0100

    added selects

--- A7-A8/LBAWselects.sql
+++ A7-A8/LBAWselects.sql
@@ -12,8 +12,8 @@ SELECT COUNT(*) FROM Person
 WHERE idPerson IN (SELECT idPerson FROM
 (SELECT * FROM SystemAdministrator) AS admins);
 
--- Numero de produtos em stock mais caros que 150� e com maior peso do que 5kg
-SELECT NULL AS "N prods 150€+ e com maior peso do que 5kg";
+-- Numero de produtos em stock mais caros que 150 e com maior peso do que 5kg
+SELECT NULL AS "N prods 150â¬+ e com maior peso do que 5kg";
 
 SELECT COUNT(*) FROM Product
 WHERE price>150 AND weight>5 AND stock>0;
@@ -25,3 +25,15 @@ SELECT name FROM Person
 WHERE idPerson IN (SELECT idPerson FROM
 (SELECT * FROM Client) AS clients) AND idPerson IN (SELECT idPerson FROM
 (SELECT * FROM WishList WHERE idProduct=6) AS prod6);
+
+Todos os checkouts de um cliente, neste caso do “Mariotti Mizuna”. É utilizado o email na procura por ser unique. 
+SELECT * FROM Checkout 
+ WHERE idPerson IN (SELECT idPerson FROM 
+  (SELECT * FROM Person WHERE name='marimizuna@gmail.com') AS clients);
+
+-- Lista de nomes dos produtos comprados pelo Mariotti Mizuna
+SELECT name FROM Product WHERE idProduct IN(
+ SELECT idProduct FROM Purchase WHERE idCheckout IN(
+  SELECT idCheckout FROM Checkout
+      WHERE idPerson IN (SELECT idPerson FROM
+          (SELECT * FROM Client WHERE email='marimizuna@gmail.com') AS clients)));
